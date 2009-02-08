b2978035baa535d0c2faa5becc81ec1624acf0dc
Neil Crosby <neil@thetenwordreview.com>
2009-02-08 15:35:39 +0000

    Now get a load of CC Hoff photos from Flickr

--- hoff_data.php
+++ hoff_data.php
@@ -3,10 +3,24 @@
 echo "var hoff_data = ";
 echo json_encode(
     array(
-        'http://hoffify.co.uk/images/hoff1.jpg',
-        'http://hoffify.co.uk/images/hoff2.jpg',
-        'http://hoffify.co.uk/images/hoff3.jpg',
-        'http://hoffify.co.uk/images/hoff4.jpg',
+        'http://farm1.static.flickr.com/43/76537458_91f55edc65_m.jpg',
+        'http://farm1.static.flickr.com/45/120648282_a555595779_m.jpg',
+        'http://farm4.static.flickr.com/3082/2747877876_733f375251_m.jpg',
+        'http://farm1.static.flickr.com/151/386688423_18ac01a022_m.jpg',
+        'http://farm3.static.flickr.com/2259/1537693538_71030607ba_m.jpg',
+        'http://farm1.static.flickr.com/81/263848755_9efe1291bc_m.jpg',
+        'http://farm4.static.flickr.com/3264/2529765305_9b84fff0be_m.jpg',
+        'http://farm1.static.flickr.com/22/34494369_f39b7fee9f_m.jpg',
+        'http://farm1.static.flickr.com/13/17965671_40714448a6_m.jpg',
+        'http://farm1.static.flickr.com/72/202011231_53854b19fc_m.jpg',
+        'http://farm4.static.flickr.com/3297/3219716276_9c9a74b3b4_m.jpg',
+        'http://farm1.static.flickr.com/179/404895638_76757e165b_m.jpg',
+        'http://farm1.static.flickr.com/125/376239395_f672bf3ffd_m.jpg',
+        'http://farm1.static.flickr.com/25/53065807_1234b1d505_m.jpg',
+        'http://farm1.static.flickr.com/37/97930404_2de9abc5f1_m.jpg',
+        'http://farm4.static.flickr.com/3239/2902437909_088c494554_m.jpg',
+        'http://farm1.static.flickr.com/30/41238555_a5683bb617_m.jpg',
+        'http://farm2.static.flickr.com/1060/1422372825_b73751daa8_m.jpg',
     )
 );
 echo ";";
